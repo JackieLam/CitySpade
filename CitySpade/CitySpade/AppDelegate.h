8ed05b0868f7351acf7df8ef6f20d1e72bd7a323
Jackie Lam <jackielin1992@gmail.com>
2014-01-20 10:12:18 +0800

    Initial

---
+++ CitySpade/CitySpade/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CitySpade
+//
+//  Created by Cho-Yeung Lam on 20/1/14.
+//  Copyright (c) 2014 Cho-Yeung Lam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
