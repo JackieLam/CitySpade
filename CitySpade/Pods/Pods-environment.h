75b86b6fa11ac6475e497e6181b5414791862d72
Jackie Lam <jackielin1992@gmail.com>
2014-01-21 10:51:25 +0800

    Add: Cocoapod - LBBlurredImage and MFSideMenu

---
+++ CitySpade/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// LBBlurredImage
+#define COCOAPODS_POD_AVAILABLE_LBBlurredImage
+#define COCOAPODS_VERSION_MAJOR_LBBlurredImage 0
+#define COCOAPODS_VERSION_MINOR_LBBlurredImage 1
+#define COCOAPODS_VERSION_PATCH_LBBlurredImage 0
+
+// MFSideMenu
+#define COCOAPODS_POD_AVAILABLE_MFSideMenu
+#define COCOAPODS_VERSION_MAJOR_MFSideMenu 0
+#define COCOAPODS_VERSION_MINOR_MFSideMenu 5
+#define COCOAPODS_VERSION_PATCH_MFSideMenu 4
+
