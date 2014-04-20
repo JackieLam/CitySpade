c46393574a386fcebf119a60c03fada6a5af4e2d
AlayshChen <chenhb36@mail2.sysu.edu.cn>
2014-04-20 23:20:20 +0800

    Cache the thumImageView

--- CitySpade/CitySpade/UI/AsynImageView.h
+++ CitySpade/CitySpade/UI/AsynImageView.h
@@ -13,12 +13,12 @@
     NSURLConnection *connection;
     NSMutableData *loadData;
 }
-//图片对应的缓存在沙河中的路径
+
 @property (nonatomic, strong) NSString *fileName;
 
-//指定默认未加载时，显示的默认图片
+
 @property (nonatomic, strong) UIImage *placeholderImage;
-//请求网络图片的URL
+
 @property (nonatomic, strong) NSString *imageURL;
 
 
